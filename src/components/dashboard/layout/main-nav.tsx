10a595e2b198ba7b10cc82f104e4a5eddc4bcc95
Davis Roldan <davisrdln@gmail.com>
2025-05-27 20:53:55 -0600

    feat: add structure project

---
+++ src/components/dashboard/layout/main-nav.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { PuzzlePiece } from '@phosphor-icons/react';
+import { usePopover } from '@/hooks/use-popover';
+
+import { MobileNav } from './mobile-nav';
+import { UserPopover } from './user-popover';
+import { Typography } from '@mui/material';
+
+export function MainNav(): React.JSX.Element {
+  const [openNav, setOpenNav] = React.useState<boolean>(false);
+
+  const userPopover = usePopover<HTMLDivElement>();
+
+  return (
+    <React.Fragment>
+      <Box
+        component="header"
+        sx={{
+          borderBottom: '1px solid var(--mui-palette-divider)',
+          backgroundColor: 'var(--mui-palette-background-paper)',
+          position: 'sticky',
+          top: 0,
+          zIndex: 'var(--mui-zIndex-appBar)',
+        }}
+      >
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ alignItems: 'center', minHeight: '64px', px: 2 }}
+        >
+          <Stack sx={{ alignItems: 'center' }} direction="row" spacing={1}>
+            <PuzzlePiece size={24} weight="bold" />
+            <Typography variant="h6">Puzzle Solver</Typography>
+          </Stack>
+        </Stack>
+      </Box>
+      <UserPopover anchorEl={userPopover.anchorRef.current} onClose={userPopover.handleClose} open={userPopover.open} />
+      <MobileNav
+        onClose={() => {
+          setOpenNav(false);
+        }}
+        open={openNav}
+      />
+    </React.Fragment>
+  );
+}
